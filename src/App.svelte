f82de4a39a3c11dfbeaa85b58cb50ab3fd0a0635
svdl <svdl@fundament.nl>
2021-12-15 21:14:24 +0100

    added UTC clock component and did some basic styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,23 @@
 <script>
-	export let name;
+	import Clock from './Clock.svelte';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>Build tick app</p>
+
+	<Clock />
+
 </main>
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
+		padding: 0;
 		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		margin: 0;
+		background-color: #1C2C34;
+		color: white;
+		min-height: 100vh;
+		min-width: 100vw;
 	}
 
 	@media (min-width: 640px) {
