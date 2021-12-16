8fd412eb4788ee757dcb14ed21790b1969da6bfd
svdl <svdl@fundament.nl>
2021-12-16 21:26:06 +0100

    Revert "added UTC clock component and did some basic styles"
    This reverts commit f82de4a39a3c11dfbeaa85b58cb50ab3fd0a0635.

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,25 @@
 <script>
-	import Clock from './Clock.svelte';
+	export let name;
 </script>
 
 <main>
-	<p>Build tick app</p>
-
-	<Clock />
-
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
 	main {
-		padding: 0;
+		text-align: center;
+		padding: 1em;
 		max-width: 240px;
-		margin: 0;
-		background-color: #1C2C34;
-		color: white;
-		min-height: 100vh;
-		min-width: 100vw;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
